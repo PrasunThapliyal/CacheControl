b9718693ed4fb324492883e9483da787ee0dbc67
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2023-02-15 18:54:30 +0530

    Initial Commit

---
+++ CacheControl/CacheControl/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CacheControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("something")]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
+        public IEnumerable<WeatherForecast> GetSomething([FromQuery] string reportName)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ CacheControl/CacheControl/Program.cs
@@ -0,0 +1,30 @@
+namespace CacheControl
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            //builder.Services.AddResponseCaching(); // Not required if we are just using public, max-age            
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseStaticFiles(); // For index.html. Need to create a folder wwwroot and put index.html there
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
